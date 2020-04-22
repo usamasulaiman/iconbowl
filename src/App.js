fd024e103c3916169841e05ddc655b7dad55b6db
Usama Sulaiman <sulaiman.usama@gmail.com>
2020-04-23 00:56:13 +0530

    first icon

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Star from './icons/star.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Star />
+    </div>
+  );
+}
+
+export default App;
